5cd8362157f2d04a82339abb9d9f8c3168ca0eb3
Pratyaksh Bharadwaj <pratyakshom.05@gmail.com>
2022-08-17 05:50:14 +0000

    sweet: add lunch choices
    Signed-off-by: Pratyaksh Bharadwaj <pratyakshom.05@gmail.com>

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -1,8 +1,13 @@
 #
-# Copyright (C) 2021 The LineageOS Project
+# Copyright (C) 2021 The Android Open Source Project
 #
 # SPDX-License-Identifier: Apache-2.0
 #
 
 PRODUCT_MAKEFILES := \
     $(LOCAL_DIR)/aosp_sweet.mk
+
+COMMON_LUNCH_CHOICES := \
+    aosp_sweet-user \
+    aosp_sweet-userdebug \
+    aosp_sweet-eng
