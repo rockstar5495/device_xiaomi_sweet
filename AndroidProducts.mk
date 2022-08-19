2ede15ac39fd94e18a488941abd054430be3eda5
Pratyaksh Bharadwaj <pratyakshom.05@gmail.com>
2022-08-19 07:04:33 +0000

    sweet: 13 bringup
    Signed-off-by: Pratyaksh Bharadwaj <pratyakshom.05@gmail.com>

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -5,9 +5,9 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/arrow_sweet.mk
+    $(LOCAL_DIR)/evolution_sweet.mk
 
 COMMON_LUNCH_CHOICES := \
-    arrow_sweet-user \
-    arrow_sweet-userdebug \
-    arrow_sweet-eng
+    evolution_sweet-user \
+    evolution_sweet-userdebug \
+    evolution_sweet-eng
--- evolution_sweet.mk
+++ evolution_sweet.mk
@@ -11,10 +11,10 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 # Inherit from sweet device
 $(call inherit-product, device/xiaomi/sweet/device.mk)
 
-# Inherit some common ArrowOS configuration
-$(call inherit-product, vendor/arrow/config/common.mk)
+# Inherit some common Evolution stuff
+$(call inherit-product, vendor/evolution/config/common_full_phone.mk)
 
-PRODUCT_NAME := arrow_sweet
+PRODUCT_NAME := evolution_sweet
 PRODUCT_DEVICE := sweet
 PRODUCT_BRAND := Redmi
 PRODUCT_MODEL := Redmi Note 10 Pro
