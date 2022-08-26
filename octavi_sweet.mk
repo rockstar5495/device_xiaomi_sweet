fa8d30218cdf63f896d48e80a33d37580253cf63
Pratyaksh Bharadwaj <pratyakshom.05@gmail.com>
2022-08-26 06:10:15 +0000

    sweet: Octavi bringup
    Signed-off-by: Pratyaksh Bharadwaj <pratyakshom.05@gmail.com>

--- octavi_sweet.mk
+++ octavi_sweet.mk
@@ -1,5 +1,5 @@
 #
-# Copyright (C) 2021 The LineageOS Project
+# Copyright (C) 2021 The OctaviOS Project
 #
 # SPDX-License-Identifier: Apache-2.0
 #
@@ -11,11 +11,11 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 # Inherit from sweet device
 $(call inherit-product, device/xiaomi/sweet/device.mk)
 
-# Inherit some common LineageOS stuff
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+# Inherit some common OctaviOS stuff
+$(call inherit-product, vendor/octavi/config/common_full_phone.mk)
 
 # Product identifer to come after all inclusions
-PRODUCT_NAME := lineage_sweet
+PRODUCT_NAME := octavi_sweet
 PRODUCT_DEVICE := sweet
 PRODUCT_BRAND := Redmi
 PRODUCT_MODEL := Redmi Note 10 Pro
