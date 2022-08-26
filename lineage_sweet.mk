1245e5267aaff453b58df735630cd23ab7347810
Pratyaksh Bharadwaj <pratyakshom.05@gmail.com>
2022-08-26 06:04:57 +0000

    sweet: add build fp & desc
    Signed-off-by: Pratyaksh Bharadwaj <pratyakshom.05@gmail.com>

--- lineage_sweet.mk
+++ lineage_sweet.mk
@@ -14,10 +14,16 @@ $(call inherit-product, device/xiaomi/sweet/device.mk)
 # Inherit some common LineageOS stuff
 $(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 
+# Product identifer to come after all inclusions
 PRODUCT_NAME := lineage_sweet
 PRODUCT_DEVICE := sweet
 PRODUCT_BRAND := Redmi
 PRODUCT_MODEL := Redmi Note 10 Pro
 PRODUCT_MANUFACTURER := Xiaomi
+
+# Build info
+BUILD_FINGERPRINT := "Redmi/sweetin/sweetin:11/RKQ1.200826.002/V12.5.8.0.RKFINXM:user/release-keys"
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    PRIVATE_BUILD_DESC="sweetin-user 11 RKQ1.200826.002 V12.5.8.0.RKFINXM release-keys"
 
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
